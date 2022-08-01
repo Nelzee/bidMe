af127eb2f35ec6dce4fe7e4278af6561585847ed
Nelzee <Nelzee07@gmail.com>
2022-08-01 05:38:07 +0200

    added header and footer components

---
+++ components/header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const header = () => {
+  return (
+    <div>
+      <div className="logo">bidMe</div>
+      <ul>
+        <li>home</li>
+        <li>products</li>
+        <li>about us</li>
+      </ul>
+    </div>
+  );
+};
+
+export default header;
